ae7d1317e9f4337b5d1c3c2c4f8d2dde6e43be2a
mizbauddintareq <mizbauddintareq@gmail.com>
2022-08-04 06:10:40 +0600

    1 file modified

--- index.js
+++ index.js
@@ -4,9 +4,41 @@ import './style.css';
 // Write Javascript code!
 console.log('Hello frineds');
 
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const syncCallbackFunction = (number, start) => {
+  const numIsPrime = isPrime(number);
+  console.log(
+    'prime: ',
+    number,
+    'result: ',
+    numIsPrime,
+    'start: ',
+    start,
+    'end',
+    new Date()
+  );
+};
+
+const sleepWithCallback = (number, callback) => {
+  const start = Date.now();
+  const sleep = number * 100;
+  while (true) {
+    if (Date.now() - start > sleep) break;
+  }
+  callback(number, new Date(start));
+};
+
 const syncWithCallback = () => {
   for (let index = 0; index < 10; index++) {
     console.log('Looping: \t', index, '\t', new Date());
+    sleepWithCallback(index, syncCallbackFunction);
   }
 };
 
